aed387a3d519d95ec8e985316401032d4c8c6dfc
Aryan Sarate <aryansarate123@gmail.com>
2026-07-22 20:35:43 +0530

    Initial commit

---
+++ Sum_Of_Even_Numbers.py
@@ -0,0 +1,15 @@
+sum=0
+for num in range(2,201):
+    if num%2==0:
+        sum=sum+num
+print(sum)
+
+sum=0
+for num in range(2,201,2):
+    if num%2==0:
+        sum=sum+num
+print(sum)
+
+n=100
+sum=n*(n+1)
+print(sum)
